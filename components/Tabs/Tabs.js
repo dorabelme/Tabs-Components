d9ca191614d0955701f7686d3682af348af2dfd2
Dora Belme <contact.dorabelme@gmail.com>
2019-06-21 10:29:56 -0400

    stretch

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -62,27 +62,72 @@ class TabItem {
 links = document.querySelectorAll(".tabs-link").forEach(link => new TabLink(link));
 
 
+// stretch solution code
+
+// class TabsItem {
+//   constructor(element) {
+//     this.element = element;
+//   }
+
+//   select() {
+//     this.element.classList.add('tabs-item-selected');
+//   }
+
+//   deselect() {
+//     this.element.classList.remove('tabs-item-selected');
+//   }
+// }
+
+// class TabsLink {
+//   constructor(element, parent) {
+//     this.element = element;
+//     this.tabs = parent;
+//     this.tabsItem = parent.getTab(this.element.dataset.tab);
+//     this.tabsItem = new TabsItem(this.tabsItem);
+//     this.element.addEventListener('click', () => {
+//       this.tabs.updateActive(this);
+//       this.select()
+//     });
+//   };
+
+//   select() {
+//     this.element.classList.add('tabs-link-selected')
+//     this.tabsItem.select();
+//   }
+
+//   deselect() {
+//     this.element.classList.remove('tabs-link-selected');
+//     this.tabsItem.deselect();
+//   }
+// }
+
+// class Tabs {
+//   constructor(element) {
+//     this.element = element;
+//     this.links = this.element.querySelectorAll('.tabs-link');
+//     this.links = Array.from(this.links).map(link => {
+//       return new TabsLink(link, this);
+//     });
+//     this.activeLink = this.links[0];
+//     this.init();
+//   }
+
+//   init() {
+//     this.activeLink.select()
+//   }
+
+//   updateActive(newActive) {
+//     this.activeLink.deselect();
+//     this.activeLink = newActive;
+//   }
+
+//   getTab(data) {
+//     return this.element.querySelector(`.tabs-item[data-tab="${data}"]`)
+//   }
+
+// }
+
+// let tabs = document.querySelectorAll('.tabs');
+// tabs = Array.from(tabs).map( tab => new Tabs(tab));
 
 
-
-
-// function tabBuilder(sample){
-
-//   const article = document.createElement("div");
-//   article.classList.add(".tabs-item");
-//   const title = document.createElement("div");
-//   title.textContent = sample.title;
-//   const description = document.createElement("div");
-//   description.classList.add(".tabs-item-description");
-//   description.textContent = sample.description;
-  
-//   container.appendChild(article);
-//   article.appendChild(h2);
-//   article.appendChild(date);
-//   article.appendChild(p1);
-//   article.appendChild(p2);
-//   article.appendChild(expandBtn);
-//   article.appendChild(closeBtn);
-
-//   const product = new Article(article);
-//   return product;
